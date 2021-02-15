4a3d7851c5d38f851264ff83caf24db0cec9e995
Lukasz <lukasz3.bala@gmail.com>
2021-02-15 10:36:49 +0100

    change author name

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 project = "adthena_task"
-author = "Your Name"
+author = "Lukasz Bala"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
 html_static_path = ["_static"]
